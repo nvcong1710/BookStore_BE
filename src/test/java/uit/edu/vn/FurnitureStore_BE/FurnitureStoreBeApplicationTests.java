bbcd9fcfcba369505acc029e363371cf39958e88
Nguyễn Viết Công <91941271+nvcong1710@users.noreply.github.com>
2024-10-05 20:55:40 +0700

    init project

---
+++ src/test/java/uit/edu/vn/FurnitureStore_BE/FurnitureStoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package uit.edu.vn.FurnitureStore_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureStoreBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
