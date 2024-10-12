b692570b7bb1cf12b5541b6a617e064edffe1662
Nguyễn Viết Công <91941271+nvcong1710@users.noreply.github.com>
2024-10-12 11:30:00 +0700

    init project

---
+++ src/main/java/uit/edu/vn/FurnitureStore_BE/FurnitureStoreBeApplication.java
@@ -0,0 +1,13 @@
+package uit.edu.vn.FurnitureStore_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureStoreBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureStoreBeApplication.class, args);
+	}
+
+}
